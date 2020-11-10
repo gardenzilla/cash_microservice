61353d1608f23d9a5fd5e8660562b678cbf9a03f
Peter Mezei <mezeipetister@gmail.com>
2020-11-10 11:37:44 +0100

    ..

---
+++ src/cash.rs
@@ -0,0 +1,123 @@
+use chrono::{DateTime, Utc};
+use packman::*;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct TransactionLog {
+    next_id: u32,
+    transactions: Vec<Transaction>,
+    balance: i32,
+}
+
+impl Default for TransactionLog {
+    fn default() -> Self {
+        TransactionLog {
+            next_id: 1,
+            transactions: Vec::new(),
+            balance: 0,
+        }
+    }
+}
+
+impl TransactionLog {
+    pub fn add_transaction(
+        &mut self,
+        kind: &str,
+        amount: i32,
+        reference: String,
+        created_by: String,
+    ) -> Result<&Transaction, String> {
+        let kind = TransactionKind::from_str(kind)?;
+        let transaction: Transaction = Transaction {
+            id: self.next_id,
+            kind,
+            amount,
+            reference,
+            created_by,
+            date_created: Utc::now(),
+        };
+        // Incerement balance
+        self.balance += transaction.amount;
+        // Increment next id
+        self.next_id += 1;
+        self.transactions.push(transaction);
+        match self.transactions.last() {
+            Some(tr) => Ok(tr),
+            None => Err(format!(
+                "Error while inserting, or getting the inserted value"
+            )),
+        }
+    }
+    pub fn get_balance(&self) -> i32 {
+        self.balance
+    }
+    pub fn get_log(&self, from: DateTime<Utc>, till: DateTime<Utc>) -> Vec<&Transaction> {
+        self.transactions
+            .iter()
+            .filter(|t| t.date_created >= from && t.date_created <= till)
+            .collect::<Vec<&Transaction>>()
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct Transaction {
+    pub(crate) id: u32,
+    pub(crate) kind: TransactionKind,
+    pub(crate) amount: i32,
+    pub(crate) reference: String,
+    pub(crate) created_by: String,
+    pub(crate) date_created: DateTime<Utc>,
+}
+
+impl Default for Transaction {
+    fn default() -> Self {
+        Transaction {
+            id: 0,
+            kind: TransactionKind::default(),
+            amount: 0,
+            reference: String::default(),
+            created_by: String::default(),
+            date_created: Utc::now(),
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub enum TransactionKind {
+    Purchase,
+    MoneyIn,
+    MoneyOut,
+}
+
+impl std::fmt::Display for TransactionKind {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            TransactionKind::Purchase => write!(f, "purchase"),
+            TransactionKind::MoneyIn => write!(f, "money_in"),
+            TransactionKind::MoneyOut => write!(f, "money_out"),
+        }
+    }
+}
+
+impl Default for TransactionKind {
+    fn default() -> Self {
+        TransactionKind::Purchase
+    }
+}
+
+impl TransactionKind {
+    fn from_str(str: &str) -> Result<TransactionKind, String> {
+        use TransactionKind::*;
+        match str {
+            "purchase" => Ok(Purchase),
+            "money_in" => Ok(MoneyIn),
+            "money_out" => Ok(MoneyOut),
+            _ => Err(format!(
+                "A tranzakció kód nem megfelelő. Az alábbi lehet: {}, {}, {}",
+                TransactionKind::Purchase,
+                TransactionKind::MoneyIn,
+                TransactionKind::MoneyOut
+            )),
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,108 @@
+extern crate gzlib;
+
+mod cash;
+use gzlib::proto::cash::cash_service_client as client;
+use gzlib::proto::cash::cash_service_server as server;
+// use gzlib::proto::user::*;
+use packman::*;
+use std::error::Error;
+use std::path::PathBuf;
+use tokio::sync::{oneshot, Mutex};
+use tonic::transport::Channel;
+use tonic::{transport::Server, Request, Response, Status};
+
+use gzlib::proto;
+
+struct CashService {
+    // Day (Date)
+    transactions: Mutex<Pack<cash::TransactionLog>>,
+}
+
+impl CashService {
+    fn init(transactions: Pack<cash::TransactionLog>) -> CashService {
+        CashService {
+            transactions: Mutex::new(transactions),
+        }
+    }
+}
+
+#[tonic::async_trait]
+impl server::CashService for CashService {
+    async fn transaction(
+        &self,
+        request: Request<proto::cash::TransactionRequest>,
+    ) -> Result<Response<proto::cash::TransactionResponse>, Status> {
+        let r = request.into_inner();
+        let tr = &mut self.transactions.lock().await;
+        let mut _tr = tr.as_mut();
+        let res = _tr
+            .unpack()
+            .add_transaction(&r.kind, r.amount, r.reference, r.created_by)
+            .map_err(|e| Status::internal(e))?;
+        Ok(Response::new(proto::cash::TransactionResponse {
+            transaction_id: res.id,
+            amount: res.amount,
+            kind: res.kind.to_string(),
+            reference: res.reference.to_string(),
+            created_at: res.date_created.to_rfc3339(),
+            created_by: res.created_by.to_string(),
+        }))
+    }
+
+    async fn get_balance(
+        &self,
+        request: Request<proto::cash::BalanceRequest>,
+    ) -> Result<Response<proto::cash::Balance>, Status> {
+        let balance = self.transactions.lock().await.unpack().get_balance();
+        Ok(Response::new(proto::cash::Balance { balance }))
+    }
+
+    async fn log(
+        &self,
+        request: Request<proto::cash::LogRequest>,
+    ) -> Result<Response<proto::cash::LogResponse>, Status> {
+        let r = request.into_inner();
+        let from = chrono::DateTime::parse_from_rfc3339(&r.from)
+            .map_err(|_| Status::invalid_argument("From date invalid"))?;
+        let till = chrono::DateTime::parse_from_rfc3339(&r.till)
+            .map_err(|_| Status::invalid_argument("Till date invalid"))?;
+        let res = self.transactions.lock().await.unpack().get_log(from, till)
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    // Init database
+    let transactions: Pack<cash::TransactionLog> =
+        Pack::load_or_init(PathBuf::from("data"), "transactions")
+            .expect("Error while loading transaction db :O");
+
+    // Address is valid?
+    // Todo: Implement auto address from environment variable
+    let addr = "[::1]:50055".parse().unwrap();
+
+    // Create shutdown channel
+    let (tx, rx) = oneshot::channel();
+
+    // Spawn the server into a runtime
+    tokio::task::spawn(async move {
+        Server::builder()
+            .add_service(server::CashServiceServer::new(CashService::init(
+                transactions,
+            )))
+            .serve_with_shutdown(addr, async {
+                let _ = rx.await;
+            })
+            .await
+            .unwrap()
+    });
+
+    tokio::signal::ctrl_c().await?;
+
+    println!("SIGINT");
+
+    // Send shutdown signal after SIGINT received
+    let _ = tx.send(());
+
+    Ok(())
+}
